9bb0c6add6ef19a2aeeb6c7b099554d58931f920
Abdelrahman0t <149137919+Abdelrahman0t@users.noreply.github.com>
2025-08-07 05:27:41 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
       <Header />
       <main>
         <Hero />
-        <Categories />
+        <Categories /> 
         <FeaturedProducts />
         <WhyChooseUs />
         <Testimonials />
