c0b2f03bd11e42d0244b94c0ab7b98dd6b8e2733
Abdelrahman0t <149137919+Abdelrahman0t@users.noreply.github.com>
2025-08-10 20:50:25 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Products from "./pages/Products";
-import ContactUs from "./pages/ContactUs";
+import Contactus from "./pages/contactus";
 import APropos from "./pages/propos";
-import Journal from "./pages/Journal";
+
 
 
 import ProductDetail from "./pages/ProductDetail";
@@ -26,8 +26,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/Journal" element={<Journal />} />
+          <Route path="/contactus" element={<Contactus />} />
           <Route path="/Apropos" element={<APropos />} />
 
           <Route path="/product/:id" element={<ProductDetail />} />
@@ -40,3 +39,4 @@ const App = () => (
 );
 
 export default App;
+
